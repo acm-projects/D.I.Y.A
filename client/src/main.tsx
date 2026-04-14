0c2b3b8a954804eaa6362fcc631db8e28e44f3fb
Polina Maidanova <polina@Polinas-MacBook-Air.local>
2026-04-13 21:33:19 -0500

    translaion.json files added for all supported languages, started the i18n config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@
 
 import { Auth0Provider } from "@auth0/auth0-react";
 import { createRoot } from 'react-dom/client';
+import "./i18n/config.ts";
 import './index.css';
 import App from './App.tsx'; // here we render App.
 // imported React Router for page navigation
