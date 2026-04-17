f1d9d9440d2c3f496eec6f4408b9f838092b6472
Polina Maidanova <polina@Polinas-MacBook-Air.local>
2026-04-16 20:20:27 -0500

    LocaleSwitcher added to change languages. Added language detector to select the language most commonly used in the browser

--- client/src/i18n/config.ts
+++ client/src/i18n/config.ts
@@ -1,10 +1,27 @@
-import i18n from "i18next";                      
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";                    
+import HttpApi from "i18next-http-backend";  
 import { initReactI18next } from "react-i18next";
 
+export const supportedLanguages = {
+    ar: "Aarabic",
+    bn: "Bengali",
+    en: "English",
+    es: "Spanish",
+    fr: "French",
+    hi: "Hindi",
+    id: "Indonesian",
+    ja: "Japanese",
+    pt: "Portuguese",
+    ru: "Russian",
+    zh: "Chinese",
+}
+
 i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({                
-    lng: "en",
     fallbackLng: "en",
     debug: true,
     interpolation: {
